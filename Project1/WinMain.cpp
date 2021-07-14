c08b16b60ade12c1f30edd67de34c728840efe91
harusame_0616 <kakino1999@gmail.com>
2021-07-14 10:27:31 +0900

    もう一度コーディングしなおした

--- Project1/WinMain.cpp
+++ Project1/WinMain.cpp
@@ -1,4 +1,4 @@
-//Windows.h����Q�[���ł͎g��Ȃ��w�b�_��r������}�N��
+﻿//Windows.hからゲームでは使わないヘッダを排除するマクロ
 #define	WIN32_LEAN_AND_MEAN
 #define	NOMINMAX
 #define	NODRAWTEXT
@@ -8,23 +8,23 @@
 #define	NOSERVICE
 #define	NOHELP
 
-//Windows API���g�����߂̃w�b�_�t�@�C��
+//Windows APIを使うためのヘッダファイル
 #include<Windows.h>
 #include<wrl.h>
 
-//DEBUG�r���h���̃��������[�N�`�F�b�N
+//DEBUGビルド時のメモリリークチェック
 #if _DEBUG
 #define _CRTDBG_MAP_ALLOC
 #include<crtdbg.h>
 #endif
 
 /*----------------------------------------------*/
-/*  C++�@�W�����C�u����                               */
+/*  C++　標準ライブラリ                               */
 /*----------------------------------------------*/
 #include<string>
 
 /*----------------------------------------------*/
-/*  DirectX�֘A�w�b�_                                     */
+/*  DirectX関連ヘッダ                                     */
 /*----------------------------------------------*/
 #include<d3d12.h>
 #include<dxgi1_6.h>
@@ -34,91 +34,91 @@
 #endif
 
 /*----------------------------------------------*/
-/*  DirectX�֘A�̃��C�u����                          */
+/*  DirectX関連のライブラリ                          */
 /*----------------------------------------------*/
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"dxguid.lib")
 
-//�������O���
+//無名名前空間
 namespace {
 
-	//�E�B���h�E�N���X�l�[���̕����z��
+	//ウィンドウクラスネームの文字配列
 	constexpr wchar_t kWindowClassName[]{ L"DirecrtX12WindowClass" };
 
-	//�^�C�g���o�[�̕\������镶����
+	//タイトルバーの表示される文字列
 	constexpr wchar_t kWindowTitle[]{ L"DirectX12 Application" };
 
-	//�f�t�H���g�̃N���C�A���g�̈�
+	//デフォルトのクライアント領域
 	constexpr int kClienWidth = 800;
 	constexpr int kClienHeight = 600;
 
-	//ComPtr��namespace�������̂ŃG�C���A�X(�ʖ�)��ݒ�
+	//ComPtrのnamespaceが長いのでエイリアス(別名)を設定
 	template<typename T>
 	using ComPtr = Microsoft::WRL::ComPtr<T>;
 
 	/*---------------------------------------------------------------------*/
-	/*  D�RD�I�u�W�F�N�g�֘A                                                                 */
+	/*  D３Dオブジェクト関連                                                                 */
 	/*---------------------------------------------------------------------*/
-	/*D3D12�f�o�C�X                                                                               */
+	/*D3D12デバイス                                                                               */
 	ComPtr<ID3D12Device> d3d12_device = nullptr;
-	//WARP�A�_�v�^�d�l�t���O(�f�o�b�O�r���h�̂ݗL��)
+	//WARPアダプタ仕様フラグ(デバッグビルドのみ有効)
 	constexpr bool kUseWarpAdapter = true;
-	//�o�b�N�o�b�t�@�̍ő�l
+	//バックバッファの最大値
 	constexpr int kMaxBackBufferSize = 3;
 
 
-	//�R�}���h���X�g,GPU�ɑ΂��ď����菇�����X�g�Ƃ��Ď���
+	//コマンドリスト,GPUに対して処理手順をリストとして持つ
 	ComPtr<ID3D12GraphicsCommandList> graphics_commandlist = nullptr;
-	//�R�}���h�A���P�[�^, �R�}���h���쐬���邽�߂̃��������m�ۂ���I�u�W�F�N�g
+	//コマンドアロケータ, コマンドを作成するためのメモリを確保するオブジェクト
 	ComPtr<ID3D12CommandAllocator> command_allocators[kMaxBackBufferSize];
-	//�R�}���h�L���[�C1�ȏ�̃R�}���h���X�g���L���[�ɐς��GPU�ɑ���
+	//コマンドキュー，1つ以上のコマンドリストをキューに積んでGPUに送る
 	ComPtr<ID3D12CommandQueue> command_queue = nullptr;
 
 
-	//�����_�[�^�[�Q�b�g�̐ݒ�(Descriptor)��VRAM�ɕۑ����邽�ߕK�v
-	ComPtr<ID3D12DescriptorHeap> rtv_descriptr_heap = nullptr;
-	//��̃����_�[�^�[�Q�b�g�f�X�N���v�^��1������̃T�C�Y���o����
+	//レンダーターゲットの設定(Descriptor)をVRAMに保存するため必要
+	ComPtr<ID3D12DescriptorHeap> rtv_descriptor_heap = nullptr;
+	//上のレンダーターゲットデスクリプタの1個当たりのサイズを覚える
 	UINT rtv_descriptr_size = 0;
 
-	//GPU�������I����Ă��邩���m�F���邽�߂ɕK�v�ȃt�F���X�I�u�W�F�N�g
+	//GPU処理が終わっているかを確認するために必要なフェンスオブジェクト
 	ComPtr<ID3D12Fence> d3d12_fence = nullptr;
-	//�t�F���X�ɏ������ޒl,�o�b�N�o�b�t�@�Ɠ�������p�ӂ��Ă���
+	//フェンスに書き込む値,バックバッファと同じ数を用意しておく
 	UINT64 fence_values[kMaxBackBufferSize]{};
-	//CPU. GPU�̓����������y�ɂƂ邽�߂Ɏg��
+	//CPU. GPUの同期処理を楽にとるために使う
 	Microsoft::WRL::Wrappers::Event fence_event;
 
 	/*---------------------------------------------------------------------*/
-	/*  �o�b�N�o�b�t�@�֘A                                                                      */
+	/*  バックバッファ関連                                                                      */
 	/*---------------------------------------------------------------------*/
-	//�X���b�v�`�F�C���I�u�W�F�N�g. �`�挋�ʂ���ʂ֏o�͂��Ă����
+	//スワップチェインオブジェクト. 描画結果を画面へ出力してくれる
 	ComPtr<IDXGISwapChain4> swap_chain = nullptr;
-	//�o�b�N�o�b�t�@�Ƃ��Ďg�������_�[�^�[�Q�b�g�z��
+	//バックバッファとして使うレンダーターゲット配列
 	ComPtr<ID3D12Resource> render_targets[kMaxBackBufferSize]{};
-	//�o�b�N�o�b�t�@�e�N�X�`���̃t�H�[�}�b�g. 1px��RGBA�e8bit����32bit
+	//バックバッファテクスチャのフォーマット. 1pxをRGBA各8bitずつの32bit
 	DXGI_FORMAT backbuffer_format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	//�쐬����o�b�N�o�b�t�@��
+	//作成するバックバッファ数
 	int backbuffer_size = 2;
-	//���݂̃o�b�N�o�b�t�@�̃C���f�b�N�X
+	//現在のバックバッファのインデックス
 	int backbuffer_index = 0;
 
 
 
-	/// @brief  �E�B���h�E�v���V�[�W��
-	/// @param hwnd  �E�B���h�E�n���h��
-	/// @param message ���b�Z�[�W�̎��
-	/// @param wParam ���b�Z�[�W�̃p�����[�^
-	/// @param lParam �@���b�Z�[�W�̃p�����[�^
-	/// @return ���b�Z�[�W�̖߂�l
+	/// @brief  ウィンドウプロシージャ
+	/// @param hwnd  ウィンドウハンドル
+	/// @param message メッセージの種類
+	/// @param wParam メッセージのパラメータ
+	/// @param lParam 　メッセージのパラメータ
+	/// @return メッセージの戻り値
 	LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 		switch (message)
 		{
-		case WM_CLOSE:	//�v���O�����I���V�O�i��
+		case WM_CLOSE:	//プログラム終了シグナル
 			DestroyWindow(hwnd);
 			break;
 
-		case WM_DESTROY:	//�E�B���h�E�j���V�O�i��
+		case WM_DESTROY:	//ウィンドウ破棄シグナル
 			PostQuitMessage(0);
 			break;
 		default:
@@ -128,17 +128,17 @@ namespace {
 		return 0;
 	}
 
-	/// @brief D3D�I�u�W�F�N�g�̏�����
-	/// @param hwnd	 �A�v���P�[�V�����̃E�B���h�E�n���h��
-	/// @return  ���� true / ���s false
+	/// @brief D3Dオブジェクトの初期化
+	/// @param hwnd	 アプリケーションのウィンドウハンドル
+	/// @return  成功 true / 失敗 false
 	bool InitializeD3D(HWND hwnd)
 	{
 		HRESULT hr = S_OK;
 
-		//�t�@�N�g���쐬�t���O
+		//ファクトリ作成フラグ
 		UINT dxgi_factory_flags = 0;
 
-		//_DEBUG�r���h����D3D12�̃f�o�b�O�@�\�̗L��������
+		//_DEBUGビルド時はD3D12のデバッグ機能の有効化する
 #if _DEBUG
 		ComPtr<ID3D12Debug> debug;
 		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug))))
@@ -146,14 +146,14 @@ namespace {
 			debug->EnableDebugLayer();
 		}
 
-		//DXGI�̃f�o�b�O�@�\�ݒ�
+		//DXGIのデバッグ機能設定
 		ComPtr<IDXGIInfoQueue> info_queue;
 		if (SUCCEEDED(DXGIGetDebugInterface1(
 			0, IID_PPV_ARGS(info_queue.GetAddressOf()))))
 		{
 			dxgi_factory_flags = DXGI_CREATE_FACTORY_DEBUG;
 
-			//DXGI�ɖ�肪���������Ƀv���O�������u���[�N����悤�ɂ���
+			//DXGIに問題があった時にプログラムがブレークするようにする
 			info_queue->SetBreakOnSeverity(
 				DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_ERROR, true);
 			info_queue->SetBreakOnSeverity(
@@ -161,7 +161,7 @@ namespace {
 		}
 #endif
 
-		//�A�_�v�^���擾���邽�߂̃t�@�N�g���쐬
+		//アダプタを取得するためのファクトリ作成
 		ComPtr<IDXGIFactory5> dxgi_factory;
 		hr = CreateDXGIFactory2(dxgi_factory_flags,
 			IID_PPV_ARGS(&dxgi_factory));
@@ -171,12 +171,12 @@ namespace {
 			return 1;
 		}
 
-		//D3D12���g����A�_�v�^�̒T������
-		ComPtr<IDXGIAdapter1> adapter;	//�A�_�v�^(�r�f�I�J�[�h)�I�u�W�F�N�g
+		//D3D12が使えるアダプタの探索処理
+		ComPtr<IDXGIAdapter1> adapter;	//アダプタ(ビデオカード)オブジェクト
 		{
 			UINT adapter_index = 0;
 
-			//dxgi_factory->EnumAdapters1�ɂ��A�_�v�^�̐��\���`�F�b�N���Ă���
+			//dxgi_factory->EnumAdapters1によりアダプタの性能をチェックしていく
 			while (DXGI_ERROR_NOT_FOUND
 				!= dxgi_factory->EnumAdapters1(adapter_index, &adapter))
 			{
@@ -189,24 +189,24 @@ namespace {
 					continue;
 				}
 
-				//D3D12�͎g�p�\��
+				//D3D12は使用可能か
 				hr = D3D12CreateDevice(adapter.Get(),
 					D3D_FEATURE_LEVEL_11_0,
 					__uuidof(ID3D12Device), nullptr);
 
 				if (SUCCEEDED(hr))
 				{
-					break;	//�g����A�_�v�^���������ꍇ�T���𒆒f���Ď��̏�����
+					break;	//使えるアダプタがあった場合探索を中断して次の処理へ
 				}
 			}
 		}
 #if _DEBUG
-		//�K�؂ȃn�[�h�E�F�A�A�_�v�^���Ȃ��āAkUseWarpAdapter��true�Ȃ�
-		//WARP�A�_�v�^���擾(WARP = Windows Advanced Rasterization + Platform)
+		//適切なハードウェアアダプタがなくて、kUseWarpAdapterがtrueなら
+		//WARPアダプタを取得(WARP = Windows Advanced Rasterization + Platform)
 		if (adapter == nullptr && kUseWarpAdapter)
 		{
-			//WARP��GPU�@�\�̈ꕔ��CPU���Ŏ��s���Ă����A�_�v�^
-			//�����ȃ\�t�g�E�F�A��萏���������{�Ԃ̃Q�[���Ŏg���͖̂���
+			//WARPはGPU機能の一部をCPU側で実行してくれるアダプタ
+			//純粋なソフトウェアより随分早いが本番のゲームで使うのは無理
 			hr = dxgi_factory->EnumWarpAdapter(
 				IID_PPV_ARGS(adapter.ReleaseAndGetAddressOf()));
 			if (FAILED(hr))
@@ -221,10 +221,10 @@ namespace {
 			return false;
 		}
 
-		//�Q�[���Ŏg���f�o�C�X�쐬
-		hr = D3D12CreateDevice(adapter.Get(),	//�f�o�C�X�����Ɏg���A�_�v�^
-			D3D_FEATURE_LEVEL_11_0,					//D3D�̋@�\���x��
-			IID_PPV_ARGS(&d3d12_device));			//�󂯎��ϐ�
+		//ゲームで使うデバイス作成
+		hr = D3D12CreateDevice(adapter.Get(),	//デバイス生成に使うアダプタ
+			D3D_FEATURE_LEVEL_11_0,					//D3Dの機能レベル
+			IID_PPV_ARGS(&d3d12_device));			//受け取る変数
 
 		if (FAILED(hr))
 		{
@@ -232,14 +232,14 @@ namespace {
 		}
 		d3d12_device->SetName(L"d3d12_device");
 
-		//�R�}���h�L���[�̍쐬
+		//コマンドキューの作成
 		{
-			//�L���[�̐ݒ�p�\����
+			//キューの設定用構造体
 			D3D12_COMMAND_QUEUE_DESC desc{};
 			desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 			desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
 
-			//�L���[�̍쐬
+			//キューの作成
 			hr = d3d12_device->CreateCommandQueue(
 				&desc,
 				IID_PPV_ARGS(command_queue.ReleaseAndGetAddressOf()));
@@ -251,22 +251,125 @@ namespace {
 			command_queue->SetName(L"command_queue");
 		}
 
+		//スワップチェイン
+		{
+			//スワップチェインの設定用構造体
+			DXGI_SWAP_CHAIN_DESC1 desc{}; desc.BufferCount = backbuffer_size; // バッファの数
+			desc.Width = kClienWidth; // バックバッファの幅
+			desc.Height = kClienHeight; // バックバッファの⾼さ
+			desc.Format = backbuffer_format; // バックバッファフォーマット
+			desc.BufferUsage =
+				+DXGI_USAGE_RENDER_TARGET_OUTPUT; // レンダーターゲットの場合はこれ
+			desc.Scaling = DXGI_SCALING_STRETCH;
+			desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+			desc.AlphaMode = DXGI_ALPHA_MODE_IGNORE;
+			desc.Flags = 0;
+			desc.SampleDesc.Count = 1;
+			desc.SampleDesc.Quality = 0;
+
+			// スワップチェインオブジェクト作成
+			ComPtr<IDXGISwapChain1> sc;
+			hr = dxgi_factory->CreateSwapChainForHwnd(
+				command_queue.Get(), // スワップチェインを作るためのコマンドキュー
+				hwnd, // 表⽰先になるウィンドウハンドル
+				&desc, // スワップチェインの設定
+				nullptr, // フルスクリーン設定 (指定しないときはnullptr)
+				nullptr, // マルチモニタ設定 (指定しないときはnullptr)
+				sc.GetAddressOf()); // 受け取る変数
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+			//IDXGISwapChain1::As関数からIDXGISwapChain4インターフェースを取得
+			hr = sc.As(&swap_chain);
+			if (FAILED(hr))
+			{
+				return false;
+			}
+		}
+
+		//レンダーターゲット用のデスクリプタヒープの作成
+		{
+			//レンダーターゲット用デスクリプタヒープの設定構造体
+			D3D12_DESCRIPTOR_HEAP_DESC desc{};
+			desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+			desc.NumDescriptors = backbuffer_size;
+			desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+			//descの内容でメモリに動的に確保してもらう
+			hr = d3d12_device->CreateDescriptorHeap(
+				&desc,
+				IID_PPV_ARGS(rtv_descriptor_heap.ReleaseAndGetAddressOf()));
+			if (FAILED(hr))
+			{
+				return false;
+			}
+			rtv_descriptor_heap->SetName(L"rtv_descriptor_heap");
+
+			//デスクリプタヒープのメモリサイズを取得
+			rtv_descriptr_size = d3d12_device->GetDescriptorHandleIncrementSize(
+				D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		}
+
+		//レンダービューを作成
+		{
+			D3D12_CPU_DESCRIPTOR_HANDLE rtv_handle =
+				rtv_descriptor_heap->GetCPUDescriptorHandleForHeapStart();
+
+			//バックバッファの数だけレンダーターゲットを作成
+			for (int i = 0; i < backbuffer_size; i++)
+			{
+				//スワップチェインからバックバッファを取得してくる
+				hr = swap_chain->GetBuffer(
+					i,
+					IID_PPV_ARGS(render_targets[i].GetAddressOf()));
+				if (FAILED(hr))
+				{
+					return false;
+				}
+				//render_targetsに名前を設定するための文字列変数
+				std::wstring name{ L"render_targets[" + std::to_wstring(i) + L"]" };
+				render_targets[i]->SetName(name.c_str());
+
+				//レンダーターゲットビューの設定用構造体
+				D3D12_RENDER_TARGET_VIEW_DESC desc = {};
+				desc.Format = backbuffer_format;
+				desc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+
+				//RTについての情報を書き込んでRTビュー作成
+				d3d12_device->CreateRenderTargetView(
+					render_targets[i].Get(),
+					&desc,
+					rtv_handle);
+				rtv_descriptor_heap->SetName(L"rtv_descriptor_heap");
+
+				//次のループに備えて書き込み先のメモリ位置をずらす
+				rtv_handle.ptr += rtv_descriptor_heap;
+			}
+		}
+
+		//初回描画に備えて、現在のバックバッファの員デスク取得
+		backbuffer_index = swap_chain->GetCurrentBackBufferIndex();
+
+		//
+
 		return true;
 	}
 
-	/// @brief  D3D�I�u�W�F�N�g�������
+	/// @brief  D3Dオブジェクト解放処理
 	void FinalizeD3D()
 	{
 
 	}
 }// namespace
 
-/// @brief Windows �A�v���̃G���g���[�|�C���g
-/// @param hInstance �C���X�^���X�n���h��,OS���猩���A�v���̊Ǘ��ԍ��݂����Ȃ���
-/// @param hPrevInstance	 �s�g�p(�ߋ���API�Ƃ̐������ێ��p)
-/// @param lpCmdLine �R�}���h���C���������n�����
-/// @param nCmdShow �E�B���h�E�\���ݒ肪�n�����
-/// @return �I���R�[�h,�ʏ��0
+/// @brief Windows アプリのエントリーポイント
+/// @param hInstance インスタンスハンドル,OSから見たアプリの管理番号みたいなもの
+/// @param hPrevInstance	 不使用(過去のAPIとの整合性維持用)
+/// @param lpCmdLine コマンドライン引数が渡される
+/// @param nCmdShow ウィンドウ表示設定が渡される
+/// @return 終了コード,通常は0
 int WINAPI wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine,
@@ -274,17 +377,17 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance,
 {
 
 #if _DEBUG
-	//���������[�N�̌��o
+	//メモリリークの検出
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	//COM�I�u�W�F�N�g���g���Ƃ��ɂ͌Ă�ł����K�v������
+	//COMオブジェクトを使うときには呼んでおく必要がある
 	if (FAILED(CoInitializeEx(NULL, COINIT_MULTITHREADED)))
 	{
 		return 1;
 	}
 
-	//�E�B���h�E�N���X
+	//ウィンドウクラス
 	WNDCLASSEX wc{};
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -296,30 +399,30 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance,
 	wc.lpszMenuName = nullptr;
 	wc.lpszClassName = kWindowClassName;
 
-	//�E�B���h�E�N���X�o�^
+	//ウィンドウクラス登録
 	if (!RegisterClassExW(&wc))
 	{
 		return 1;
 	}
 
-	//�E�B���h�E�̕\���X�^�C���t���O,OVERLAPPEWINDOW�͈�ʓI�ȃX�^�C���ɂȂ�
+	//ウィンドウの表示スタイルフラグ,OVERLAPPEWINDOWは一般的なスタイルになる
 	DWORD window_style = WS_OVERLAPPEDWINDOW;
 
-	//�`��̈�Ƃ��ẴT�C�Y���w��
+	//描画領域としてのサイズを指定
 	RECT window_rect{
 		0,
 		0,
 		kClienWidth,
 		kClienHeight };
 
-	//�E�B���h�E�X�^�C�����܂߂��E�B���h�E�T�C�Y���v�Z����
+	//ウィンドウスタイルを含めたウィンドウサイズを計算する
 	AdjustWindowRect(&window_rect, window_style, FALSE);
 
-	//�E�B���h�E�̎��T�C�Y���v�Z
+	//ウィンドウの実サイズを計算
 	auto window_width = window_rect.right - window_rect.left;
 	auto window_height = window_rect.bottom - window_rect.top;
 
-	//�E�B���h�E�쐬
+	//ウィンドウ作成
 	HWND hwnd = CreateWindowExW(
 		0, kWindowClassName,
 		kWindowTitle,
@@ -338,7 +441,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance,
 		return 1;
 	}
 
-	//�E�B���h�E��\��
+	//ウィンドウを表示
 	ShowWindow(hwnd, nCmdShow);
 
 	if (InitializeD3D(hwnd) == false)
@@ -346,24 +449,25 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance,
 		return 1;
 	}
 
-	//���C�����[�v
-	//WM_QUIT��������v���O�����̏I��
+	//メインループ
+	//WM_QUITがきたらプログラムの終了
 	for (MSG msg{}; WM_QUIT != msg.message;)
 	{
-		//MSG�\���̂��g���ă��b�Z�[�W���󂯎��
+		//MSG構造体を使ってメッセージを受け取る
 		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
-			//���b�Z�[�W������Ή��̂Q�s�Ń��b�Z�[�W�v���V�[�W��
+			//メッセージがあれば下の２行でメッセージプロシージャ
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 		else
 		{
-			//�����ɃQ�[���̏���������
+			//ここにゲームの処理を書く
 		}
 	}
 
 	FinalizeD3D();
+	CoUninitialize();
 
 	return 0;
 }
