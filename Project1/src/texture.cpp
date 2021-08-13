9aabdcacde1535ce7eb11e634c4ec1fff411bed2
harusame_0616 <kakino1999@gmail.com>
2021-08-13 20:09:27 +0900

    TextureViewデスクリプタ 作成完了

--- Project1/src/texture.cpp
+++ Project1/src/texture.cpp
@@ -159,4 +159,150 @@ namespace ncc {
 
 		return true;
 	}
+
+	bool TextureUploadCommandList::AddList(TextureResource* texture)
+	{
+		// 不正な状態なら失敗
+		if (texture->is_upload_wait_ == false ||
+			texture->is_enabled_ == true ||
+			is_begin_recording_ == false)
+		{
+			return false;
+		}
+
+		// ステージングバッファ作成
+		ComPtr<ID3D12Resource> staging_buffer;
+
+		D3D12_PLACED_SUBRESOURCE_FOOTPRINT layouts[1]{}; // サブリソース毎のメモリのデータ形式
+		UINT num_rows[1]{};	//	テクスチャ内のサブリソース毎の行数を受け取る
+		UINT64 row_size_bytes[1]{}; // サブリソース毎の1行当たりのバイトサイズ
+		UINT64 total_bytes = 0;	//	テクスチャ全体のバイトサイズ
+
+		{
+			D3D12_RESOURCE_DESC resource_desc = texture->resource_->GetDesc();
+
+			// バッファにコピーするときに必要になる情報を取得
+			device_->GetCopyableFootprints(
+				&resource_desc,
+				0,
+				1,
+				0,
+				layouts,
+				num_rows,
+				row_size_bytes,
+				&total_bytes);
+
+			// ステージングバッファ用のヒープ設定
+			D3D12_HEAP_PROPERTIES buffer_heap{};
+			buffer_heap.Type = D3D12_HEAP_TYPE_UPLOAD;	//	CPUから書き込み可能にする
+			buffer_heap.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+			buffer_heap.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+			buffer_heap.CreationNodeMask = 1;
+			buffer_heap.VisibleNodeMask = 1;
+
+			// ステージングバッファのリソース設定
+			D3D12_RESOURCE_DESC buffer_desc{};
+			buffer_desc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+			buffer_desc.Alignment = 0;
+			buffer_desc.Width = total_bytes; // バッファ全体のサイズ
+			buffer_desc.Height = 1;
+			buffer_desc.DepthOrArraySize = 1;
+			buffer_desc.MipLevels = 1;
+			buffer_desc.Format = DXGI_FORMAT_UNKNOWN;
+			buffer_desc.SampleDesc.Count = 1;
+			buffer_desc.SampleDesc.Quality = 0;
+			buffer_desc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+			buffer_desc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+			// ステージングバッファ作成
+			if (FAILED(device_->CreateCommittedResource(
+				&buffer_heap,
+				D3D12_HEAP_FLAG_NONE,
+				&buffer_desc,
+				D3D12_RESOURCE_STATE_GENERIC_READ,
+				nullptr,
+				IID_PPV_ARGS(&staging_buffer))))
+			{
+				return false;
+			}
+		}
+
+		//	ステージングバッファへテクスチャをコピー
+		{
+			// バッファをMapして書き込みアドレスをもらう
+			void* buffer_ptr = nullptr;
+			if (FAILED(staging_buffer->Map(
+				0,
+				nullptr,	// D3D12_RANGE, 実は全体をMapするときはnullptrでOK
+				&buffer_ptr)))
+			{
+				return false;
+			}
+
+			// コピー元のアドレス等を準備
+			const std::uint8_t* src_data = static_cast<const std::uint8_t*>(texture->subresource_.pData);
+			LONG_PTR src_row_pitch = texture->subresource_.RowPitch;
+			UINT copy_size = static_cast<UINT>(row_size_bytes[0]);
+
+			// システムメモリにある画像からバッファへ、１行毎にメモリコピー
+			for (UINT row = 0; row < num_rows[0]; ++row)
+			{
+				void* dest_ptr = static_cast<std::uint8_t*>(buffer_ptr) + (layouts[0].Footprint.RowPitch * row);
+				const void* src_ptr = src_data + (src_row_pitch * row);
+
+				memcpy(dest_ptr, src_ptr, copy_size);
+			}
+			staging_buffer->Unmap(0, nullptr); //コピー終了
+		}
+
+		// ステージングバッファからリソースへのコピーコマンド作成
+		{
+			D3D12_TEXTURE_COPY_LOCATION src{};
+			D3D12_TEXTURE_COPY_LOCATION dst{};
+
+			// コピー元(テクスチャリソース)の設定
+			src.pResource = staging_buffer.Get();
+			src.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
+			src.PlacedFootprint = layouts[0];
+
+			// コピーコマンド
+			dst.pResource = texture->resource_.Get();
+			dst.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
+			dst.SubresourceIndex = 0;
+
+			// コピーコマンド
+			command_list_->CopyTextureRegion(&dst, 0, 0, 0, &src, nullptr);
+
+			D3D12_RESOURCE_BARRIER barrier{};
+			barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+			barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+			barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
+			barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+
+			// バリアコマンド
+			command_list_->ResourceBarrier(1, &barrier);
+		}
+
+		// 転送成功時にテクスチャリソースを有効化させるため覚えておく
+		upload_list_.emplace_back(texture);
+
+		buffers_.emplace_back(staging_buffer);
+		
+		return true;
+	}
+
+	/*-------------------------------------------*/
+	/* TextureView                                            */
+	/*-------------------------------------------*/
+	TextureView::~TextureView()
+	{
+		Release();
+	}
+
+	bool TextureView::Create(Microsoft::WRL::ComPtr<ID3D12Device>device,
+		DescriptorHeap* srv_heap, TextureResource* texture)
+	{
+
+	}
+
 }
