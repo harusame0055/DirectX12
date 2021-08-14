e75391d4f6c1d3f8a2a0a97a697293f9e9025293
harusame_0616 <kakino1999@gmail.com>
2021-08-14 21:21:06 +0900

    sprite_render.h 書き込み

--- Project1/src/sprite_render.h
+++ Project1/src/sprite_render.h
@@ -12,3 +12,39 @@
 #include"descriptor_heap.h"
 #include"sprite.h"
 
+namespace ncc {
+
+	/// @brief スプライト描画オブジェクト
+
+	class SpriteRender {
+	public:
+
+		/// @brief レンダラの初期化
+		/// @param device 
+		/// @param buffering_count 
+		/// @retval true 初期化成功 
+		/// @retval false 初期化失敗 
+		bool Initialize(ID3D12Device* device, int buffering_count, const D3D12_VIEWPORT& viewport);
+
+		/// @brief レンダラ終了処理
+		void Finalize();
+
+		/// @brief スプライトの描画登録処理,実際にはまだ描画してない．
+		/// @param sprite 描画スプライト
+		void Draw(Sprite& sprite);
+
+		/// @brief 
+		/// @param texture 
+		/// @param texture_size 
+		/// @param position 
+		/// @param rotation 
+		/// @param color 
+		/// @param cell 
+		void Draw(D3D12_GPU_DESCRIPTOR_HANDLE texture,
+			const DirectX::XMUINT2& texture_size,
+			const DirectX::XMFLOAT3& position,
+			const float& rotation,
+			const DirectX::XMFLOAT4& color,
+			const Cell* cell);
+	};
+}
